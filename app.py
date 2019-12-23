c2b29a14dc81369d6d8cb4defe393292b0d6f96b
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-12-23 18:17:44 +0800

    Add index template

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html", **locals())
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
