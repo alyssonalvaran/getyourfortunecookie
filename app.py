dd06d0d47cdac114d9ad3d794e30d0b3ea7c88c8
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-12-23 19:09:29 +0800

    Add cookie sayings

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def home():
-    return render_template("index.html", **locals())
+	with open("static/temp/cookie-sayings.txt", "r") as f:
+		details = f.read()
+		print(details)
+		f.close()
+
+	return render_template("index.html", **locals())
+
+@app.route("/cookie-sayings/")
+def send_cookie_sayings():
+	return app.send_static_file('temp/cookie-sayings.txt')
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
