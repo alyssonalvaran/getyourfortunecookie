085a024f5eebb689580737f57d7d67bc9040b106
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-12-23 18:03:09 +0800

    Add __name__ condition to run app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
