eefbada407889e6d039f6a9559f4b0514bc9fcb4
Alysson Alvaran <alvaran.alysson@gmail.com>
2019-12-23 19:23:21 +0800

    Add fortune string and try again button

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 from flask import Flask, render_template, send_from_directory
+from random import randrange
+
 
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def home():
-	with open("static/temp/cookie-sayings.txt", "r") as f:
-		details = f.read()
-		print(details)
+	# open cookie sayings file
+	file_path = "static/temp/cookie-sayings.txt"
+	with open(file_path, "r") as f:
+		sayings = f.read()
+		sayings_list = sayings.split("\n")
 		f.close()
+
+	# pick random fortune
+	random_num = randrange(len(sayings_list))
+	fortune = sayings_list[random_num]
 
 	return render_template("index.html", **locals())
 
 @app.route("/cookie-sayings/")
 def send_cookie_sayings():
-	return app.send_static_file('temp/cookie-sayings.txt')
+	return app.send_static_file("temp/cookie-sayings.txt")
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
